eb0649a60c83d482d1700b42d956913996629feb
Richard <richard.rabillon@live.fr>
2016-03-30 16:58:40 +0200

    Readded htacces

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+define('SQL_HOST', 'localhost');
+define('SQL_USER', 'root');
+define('SQL_PASS', '');
+define('SQL_DBNAME', 'iesa');
+try {
+	$dbh = new PDO("mysql:dbname=".SQL_DBNAME.";host=".SQL_HOST,SQL_USER,SQL_PASS."");
+}
+catch (Exception $e) {
+	die("Erreur : ".$e->getMessage());
+}
+?>
